7e1ac78a821c502b5c3d59cc4c4c67aedda39eb6
Gerhard Theunissen <gerhardt@saiba.org.za>
2016-08-09 21:44:11 +0200

    Initial Upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MovieZone | Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+    </head>
+    <body>
+        
+        <header class="Header Utility__Wrapper">
+            <h1 class="Header__Logo">Movie<span>Zone</span></h1>
+            <nav class="Header__Navigation">
+                <a href="#">Home</a>
+                <a href="#">Courses</a>
+                <a href="#">News</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </header>
+
+        <section class="Banner">
+            <div class="Banner__Course">
+                <div class="Banner__Course__Poster">
+                    <img src="/images/movies/deadpool.jpg" alt="Deadpool Poster" class="Banner__Course__Poster__Cover">
+                    <img src="/images/play_button.png" alt="Play Trailer" class="Banner__Course__Poster__Play">
+                </div>
+                <div class="Banner__Course__Content">
+                    <h2 class="Banner__Course__Title">Deadpool</h2>
+                    <div class="Banner__Course__Ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star Utility__Muted"></i>
+                    </div>
+                    <p class="Banner__Course__Description">
+                        A former Special Forces operative turned mercenary is subjected to a rogue experiment that leaves him with accelerated healing powers, adopting the alter ego Deadpool.
+                    </p>
+                    <p class="Banner__Course__Info">
+                        R <span>|</span> 1h 48min <span>|</span> Action, Adventure, Comedy <span>|</span> 12 February 2016 (USA)
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="Courses">
+            <div class="Courses__Wrapper Utility__Wrapper">
+                <header class="Courses__Header">
+                    <h2 class="Courses__Header__Title">Popular Courses</h2>
+                    <p class="Courses__Header__View_All">View Courses</p>
+                </header>
+                <div class="Courses__Collection">
+                    <div class="Course">
+                        <img src="/images/movies/the-martian.jpg" alt="The Martian" class="Course__Image">
+                        <h3 class="Course__Title">
+                        The Martian
+                        </h3>
+                        <p class="Course__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Course">
+                        <img src="/images/movies/avengers.jpg" alt="Avengers" class="Course__Image">
+                        <h3 class="Course__Title">
+                        Avengers
+                        </h3>
+                        <p class="Course__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Course">
+                        <img src="/images/movies/inside-out.jpg" alt="Inside Out" class="Course__Image">
+                        <h3 class="Course__Title">
+                        Inside Out
+                        </h3>
+                        <p class="Course__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Course">
+                        <img src="/images/movies/mad-max.jpg" alt="The Martian" class="Course__Image">
+                        <h3 class="Course__Title">
+                        Mad Max
+                        </h3>
+                        <p class="Course__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="Lessons">
+            <div class="Lessons__Wrapper Utility__Wrapper">
+                <header class="Lessons__Header">
+                    <h2 class="Lessons__Header__Title">Popular Lessons</h2>
+                    <p class="Lessons__Header__View_All">View Lessons</p>
+                </header>
+                <div class="Lessons__Collection">
+                    
+                    <div class="Lesson">
+                        <img src="/images/movies/the-martian.jpg" alt="The Martian" class="Lesson__Image">
+                        <h3 class="Lesson__Title">
+                        The Martian
+                        </h3>
+                        <p class="Lesson__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Lesson">
+                        <img src="/images/movies/avengers.jpg" alt="Avengers" class="Lesson__Image">
+                        <h3 class="Lesson__Title">
+                        Avengers
+                        </h3>
+                        <p class="Lesson__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Lesson">
+                        <img src="/images/movies/inside-out.jpg" alt="Inside Out" class="Lesson__Image">
+                        <h3 class="Lesson__Title">
+                        Inside Out
+                        </h3>
+                        <p class="Lesson__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                    <div class="Lesson">
+                        <img src="/images/movies/mad-max.jpg" alt="The Martian" class="Lesson__Image">
+                        <h3 class="Lesson__Title">
+                        Mad Max
+                        </h3>
+                        <p class="Lesson__Info">
+                            R5,750.00 <span>|</span> 9 Months <span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="Newsletter">
+            <div class="Newsletter__Wrapper">
+
+                <h2 class="Newsletter__Title">
+                    Get the best Courses and Lessons straight in your inbox each week.
+                </h2>
+
+                <div class="Newsletter__Signup_Form">
+                    <input type="text" placeholder="First Name">
+                    <input type="email" placeholder="Email Address">
+                    <input type="submit" value="Subscribe">
+                </div>
+
+            </div>
+        </section>
+
+        <footer class="Footer">
+        
+            <p class="Footer__Logo">
+                Movie<span>Zone</span>
+            </p>
+
+            <p class="Footer__Copyright">
+                &copy; 2016 MovieZone. All Rights Reserved.
+            </p>
+
+            <p class="Footer__Links">
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+            </p>
+
+        </footer>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
